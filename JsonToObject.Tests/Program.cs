2f0d00e383c9fb07ab04d110b28726bfe9142e86
shibox <jarvey.create@163.com>
2016-05-25 11:12:19 +0800

    创建项目

---
+++ JsonToObject.Tests/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonToObject.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = "[{'IsFacet':false,'IsSegment':true,'Weigh':1,'FieldOption':67,'Data':'数据库也出“Windows”，数据“砖”家？你也行','Name':'title','Type':0,'Flag':0}]";
+            string classCode = JsonClassGenerator.GenerateString(json);
+            Console.WriteLine(classCode);
+            Console.ReadLine();
+        }
+    }
+}
